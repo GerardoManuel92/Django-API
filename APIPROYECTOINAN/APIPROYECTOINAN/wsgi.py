dad6b4eebedfc3ad1dc838809761943d3ba62a1b
GerardoManuel92 <gg204872@gmail.com>
2023-10-28 13:43:03 -0600

    modificaciones wsgi.py

--- APIPROYECTOINAN/APIPROYECTOINAN/wsgi.py
+++ APIPROYECTOINAN/APIPROYECTOINAN/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'APIPROYECTOINAN.settings')
+os.environ["DJANGO_SETTINGS_MODULE"] = 'APIPROYECTOINAN.settings'
+
+""" os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'APIPROYECTOINAN.settings') """
 
 application = get_wsgi_application()
